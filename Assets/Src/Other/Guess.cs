a7cd4f6dbd6695b12871a7dca96672659a7cd829
riguang.l <416143146@qq.com>
2017-08-03 16:19:50 +0800

    新项目

---
+++ Assets/Src/Other/Guess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guess : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    /// <summary>
+    /// 猴子吃桃，单数扔掉一个
+    /// </summary>
+    int Eat(int num) {
+        int temp = 0;
+        while (num > 0) {
+            if (num % 2 == 1) {
+                temp++;
+            }
+            num = num / 2;
+        }
+        return temp;
+    }
+
+}
