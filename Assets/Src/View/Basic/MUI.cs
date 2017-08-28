d59f4e06ee1cd2732c08991911bda7a69d1efc6f
riguang.l <416143146@qq.com>
2017-08-28 15:27:50 +0800

    简单的UI架构

---
+++ Assets/Src/View/Basic/MUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Src.View.Data;
+
+public class MUI : MonoBehaviour {
+
+    MUIData data;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
