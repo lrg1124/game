f4ada08a5f86658c16f743c8dbaa0ea8fb853ea4
riguang.l <416143146@qq.com>
2018-06-07 17:43:14 +0800

    ..

--- Assets/Src/Basic/Editor/Menu/MenuExtends.cs
+++ Assets/Src/Basic/Editor/Menu/MenuExtends.cs
@@ -4,25 +4,25 @@ using UnityEngine.UI;
 
 public class MenuExtends{
 
-    [MenuItem("GameObject/UI/Image")]
-    static void CreatImage() {
-        if (Selection.activeTransform) {
-            if (Selection.activeTransform.GetComponentInParent<Canvas>()) {
-                GameObject go = new GameObject("Image", typeof(Image));
-                go.GetComponent<Image>().raycastTarget = false;
-                go.transform.SetParent(Selection.activeTransform);
-            }
-        }
-    }
+    //[MenuItem("GameObject/UI/Image")]
+    //static void CreatImage() {
+    //    if (Selection.activeTransform) {
+    //        if (Selection.activeTransform.GetComponentInParent<Canvas>()) {
+    //            GameObject go = new GameObject("Image", typeof(Image));
+    //            go.GetComponent<Image>().raycastTarget = false;
+    //            go.transform.SetParent(Selection.activeTransform);
+    //        }
+    //    }
+    //}
 
-    [MenuItem("GameObject/UI/Text")]
-    static void CreatText() {
-        if (Selection.activeTransform) {
-            if (Selection.activeTransform.GetComponentInParent<Canvas>()) {
-                GameObject go = new GameObject("MText", typeof(MText));
-                go.transform.SetParent(Selection.activeTransform);
-            }
-        }
-    }
+    //[MenuItem("GameObject/UI/Text")]
+    //static void CreatText() {
+    //    if (Selection.activeTransform) {
+    //        if (Selection.activeTransform.GetComponentInParent<Canvas>()) {
+    //            GameObject go = new GameObject("MText", typeof(MText));
+    //            go.transform.SetParent(Selection.activeTransform);
+    //        }
+    //    }
+    //}
 
 }
---
+++ Assets/Src/Basic/Net/NetService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+namespace Assets.Src.Basic.Net {
+
+    class NetService {
+
+        Socket mSocket;
+
+        public void Connect() {
+            //mSocket = new Socket(AddressFamily.InterNetwork);
+            
+        }
+
+        public void Send() {
+
+        }
+
+
+    }
+}
